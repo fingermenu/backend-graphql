422060647582eb7f341a33acdca0c76013de1abc
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-01-21 13:03:36 +1300

    Added menu item price and choice item price loaders

--- dist/type/MenuItemPrice.js
+++ dist/type/MenuItemPrice.js
@@ -11,6 +11,10 @@ var _parseServerCommon = require('@fingermenu/parse-server-common');
 
 var _interface = require('../interface');
 
+var _ChoiceItemPrice = require('./ChoiceItemPrice');
+
+var _ChoiceItemPrice2 = _interopRequireDefault(_ChoiceItemPrice);
+
 var _MenuItem = require('./MenuItem');
 
 var _MenuItem2 = _interopRequireDefault(_MenuItem);
@@ -40,8 +44,8 @@ var getMenuItemPrice = exports.getMenuItemPrice = function () {
   };
 }();
 
-exports.default = new _graphql.GraphQLObjectType({
-  name: 'MenuItemPrice',
+var BeServedWithMenuItemPrice = new _graphql.GraphQLObjectType({
+  name: 'BeServedWithMenuItemPrice',
   fields: {
     id: {
       type: new _graphql.GraphQLNonNull(_graphql.GraphQLID),
@@ -162,6 +166,207 @@ exports.default = new _graphql.GraphQLObjectType({
 
         return function resolve(_x7, _x8, _x9) {
           return _ref6.apply(this, arguments);
+        };
+      }()
+    },
+    choiceItemPrices: {
+      type: new _graphql.GraphQLList(new _graphql.GraphQLNonNull(_ChoiceItemPrice2.default)),
+      resolve: function () {
+        var _ref8 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee7(_, args, _ref9) {
+          var dataLoaders = _ref9.dataLoaders;
+          return regeneratorRuntime.wrap(function _callee7$(_context7) {
+            while (1) {
+              switch (_context7.prev = _context7.next) {
+                case 0:
+                  return _context7.abrupt('return', dataLoaders.choiceItemPriceLoaderById.loadMany(_.get('choiceItemPriceIds').toArray()));
+
+                case 1:
+                case 'end':
+                  return _context7.stop();
+              }
+            }
+          }, _callee7, undefined);
+        }));
+
+        return function resolve(_x10, _x11, _x12) {
+          return _ref8.apply(this, arguments);
+        };
+      }()
+    }
+  },
+  interfaces: [_interface.NodeInterface]
+});
+
+exports.default = new _graphql.GraphQLObjectType({
+  name: 'MenuItemPrice',
+  fields: {
+    id: {
+      type: new _graphql.GraphQLNonNull(_graphql.GraphQLID),
+      resolve: function resolve(_) {
+        return _.get('id');
+      }
+    },
+    currentPrice: {
+      type: _graphql.GraphQLFloat,
+      resolve: function () {
+        var _ref10 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee8(_) {
+          return regeneratorRuntime.wrap(function _callee8$(_context8) {
+            while (1) {
+              switch (_context8.prev = _context8.next) {
+                case 0:
+                  return _context8.abrupt('return', _.get('currentPrice'));
+
+                case 1:
+                case 'end':
+                  return _context8.stop();
+              }
+            }
+          }, _callee8, undefined);
+        }));
+
+        return function resolve(_x13) {
+          return _ref10.apply(this, arguments);
+        };
+      }()
+    },
+    wasPrice: {
+      type: _graphql.GraphQLFloat,
+      resolve: function () {
+        var _ref11 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee9(_) {
+          return regeneratorRuntime.wrap(function _callee9$(_context9) {
+            while (1) {
+              switch (_context9.prev = _context9.next) {
+                case 0:
+                  return _context9.abrupt('return', _.get('wasPrice'));
+
+                case 1:
+                case 'end':
+                  return _context9.stop();
+              }
+            }
+          }, _callee9, undefined);
+        }));
+
+        return function resolve(_x14) {
+          return _ref11.apply(this, arguments);
+        };
+      }()
+    },
+    validFrom: {
+      type: _graphql.GraphQLString,
+      resolve: function () {
+        var _ref12 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee10(_) {
+          return regeneratorRuntime.wrap(function _callee10$(_context10) {
+            while (1) {
+              switch (_context10.prev = _context10.next) {
+                case 0:
+                  return _context10.abrupt('return', _.get('validFrom'));
+
+                case 1:
+                case 'end':
+                  return _context10.stop();
+              }
+            }
+          }, _callee10, undefined);
+        }));
+
+        return function resolve(_x15) {
+          return _ref12.apply(this, arguments);
+        };
+      }()
+    },
+    validUntil: {
+      type: _graphql.GraphQLString,
+      resolve: function () {
+        var _ref13 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee11(_) {
+          return regeneratorRuntime.wrap(function _callee11$(_context11) {
+            while (1) {
+              switch (_context11.prev = _context11.next) {
+                case 0:
+                  return _context11.abrupt('return', _.get('validUntil'));
+
+                case 1:
+                case 'end':
+                  return _context11.stop();
+              }
+            }
+          }, _callee11, undefined);
+        }));
+
+        return function resolve(_x16) {
+          return _ref13.apply(this, arguments);
+        };
+      }()
+    },
+    menuItem: {
+      type: _MenuItem2.default,
+      resolve: function () {
+        var _ref14 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee12(_, args, _ref15) {
+          var dataLoaders = _ref15.dataLoaders;
+          return regeneratorRuntime.wrap(function _callee12$(_context12) {
+            while (1) {
+              switch (_context12.prev = _context12.next) {
+                case 0:
+                  return _context12.abrupt('return', dataLoaders.menuItemLoaderById.load(_.get('menuItemId')));
+
+                case 1:
+                case 'end':
+                  return _context12.stop();
+              }
+            }
+          }, _callee12, undefined);
+        }));
+
+        return function resolve(_x17, _x18, _x19) {
+          return _ref14.apply(this, arguments);
+        };
+      }()
+    },
+    toBeServedWithMenuItemPrices: {
+      type: new _graphql.GraphQLList(new _graphql.GraphQLNonNull(BeServedWithMenuItemPrice)),
+      resolve: function () {
+        var _ref16 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee13(_, args, _ref17) {
+          var dataLoaders = _ref17.dataLoaders;
+          return regeneratorRuntime.wrap(function _callee13$(_context13) {
+            while (1) {
+              switch (_context13.prev = _context13.next) {
+                case 0:
+                  return _context13.abrupt('return', dataLoaders.menuItemPriceLoaderById.loadMany(_.get('toBeServedWithMenuItemPriceIds').toArray()));
+
+                case 1:
+                case 'end':
+                  return _context13.stop();
+              }
+            }
+          }, _callee13, undefined);
+        }));
+
+        return function resolve(_x20, _x21, _x22) {
+          return _ref16.apply(this, arguments);
+        };
+      }()
+    },
+    choiceItemPrices: {
+      type: new _graphql.GraphQLList(new _graphql.GraphQLNonNull(_ChoiceItemPrice2.default)),
+      resolve: function () {
+        var _ref18 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee14(_, args, _ref19) {
+          var dataLoaders = _ref19.dataLoaders;
+          return regeneratorRuntime.wrap(function _callee14$(_context14) {
+            while (1) {
+              switch (_context14.prev = _context14.next) {
+                case 0:
+                  return _context14.abrupt('return', dataLoaders.choiceItemPriceLoaderById.loadMany(_.get('choiceItemPriceIds').toArray()));
+
+                case 1:
+                case 'end':
+                  return _context14.stop();
+              }
+            }
+          }, _callee14, undefined);
+        }));
+
+        return function resolve(_x23, _x24, _x25) {
+          return _ref18.apply(this, arguments);
         };
       }()
     }
--- src/type/MenuItemPrice.js
+++ src/type/MenuItemPrice.js
@@ -1,11 +1,47 @@
 // @flow
 
-import { GraphQLID, GraphQLFloat, GraphQLObjectType, GraphQLString, GraphQLNonNull } from 'graphql';
+import { GraphQLID, GraphQLList, GraphQLFloat, GraphQLObjectType, GraphQLString, GraphQLNonNull } from 'graphql';
 import { MenuItemPriceService } from '@fingermenu/parse-server-common';
 import { NodeInterface } from '../interface';
+import ChoiceItemPrice from './ChoiceItemPrice';
 import MenuItem from './MenuItem';
 
 export const getMenuItemPrice = async (menuItemPriceId, sessionToken) => new MenuItemPriceService().read(menuItemPriceId, null, sessionToken);
+
+const BeServedWithMenuItemPrice = new GraphQLObjectType({
+  name: 'BeServedWithMenuItemPrice',
+  fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+      resolve: _ => _.get('id'),
+    },
+    currentPrice: {
+      type: GraphQLFloat,
+      resolve: async _ => _.get('currentPrice'),
+    },
+    wasPrice: {
+      type: GraphQLFloat,
+      resolve: async _ => _.get('wasPrice'),
+    },
+    validFrom: {
+      type: GraphQLString,
+      resolve: async _ => _.get('validFrom'),
+    },
+    validUntil: {
+      type: GraphQLString,
+      resolve: async _ => _.get('validUntil'),
+    },
+    menuItem: {
+      type: MenuItem,
+      resolve: async (_, args, { dataLoaders }) => dataLoaders.menuItemLoaderById.load(_.get('menuItemId')),
+    },
+    choiceItemPrices: {
+      type: new GraphQLList(new GraphQLNonNull(ChoiceItemPrice)),
+      resolve: async (_, args, { dataLoaders }) => dataLoaders.choiceItemPriceLoaderById.loadMany(_.get('choiceItemPriceIds').toArray()),
+    },
+  },
+  interfaces: [NodeInterface],
+});
 
 export default new GraphQLObjectType({
   name: 'MenuItemPrice',
@@ -33,6 +69,14 @@ export default new GraphQLObjectType({
     menuItem: {
       type: MenuItem,
       resolve: async (_, args, { dataLoaders }) => dataLoaders.menuItemLoaderById.load(_.get('menuItemId')),
+    },
+    toBeServedWithMenuItemPrices: {
+      type: new GraphQLList(new GraphQLNonNull(BeServedWithMenuItemPrice)),
+      resolve: async (_, args, { dataLoaders }) => dataLoaders.menuItemPriceLoaderById.loadMany(_.get('toBeServedWithMenuItemPriceIds').toArray()),
+    },
+    choiceItemPrices: {
+      type: new GraphQLList(new GraphQLNonNull(ChoiceItemPrice)),
+      resolve: async (_, args, { dataLoaders }) => dataLoaders.choiceItemPriceLoaderById.loadMany(_.get('choiceItemPriceIds').toArray()),
     },
   },
   interfaces: [NodeInterface],
