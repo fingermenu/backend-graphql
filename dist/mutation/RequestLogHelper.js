0e977ba7541eb11af7fc2485866c802ebbeadf4d
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-05-04 19:37:30 +1200

    Added config to enable request logging

--- dist/mutation/RequestLogHelper.js
+++ dist/mutation/RequestLogHelper.js
@@ -13,33 +13,49 @@ function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, a
 var logUserRequest = function () {
   var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(_ref2, requestType, _ref3, sessionToken) {
     var appVersion = _ref2.appVersion;
-    var userLoaderBySessionToken = _ref3.userLoaderBySessionToken;
+    var userLoaderBySessionToken = _ref3.userLoaderBySessionToken,
+        configLoader = _ref3.configLoader;
+    var enableLogRequest;
     return regeneratorRuntime.wrap(function _callee$(_context) {
       while (1) {
         switch (_context.prev = _context.next) {
           case 0:
+            _context.next = 2;
+            return configLoader.load('enableLogRequest');
+
+          case 2:
+            enableLogRequest = _context.sent;
+
+            if (enableLogRequest) {
+              _context.next = 5;
+              break;
+            }
+
+            return _context.abrupt('return');
+
+          case 5:
             _context.t0 = new _parseServerCommon.RequestLogService();
             _context.t1 = _immutable.Map;
             _context.t2 = appVersion;
             _context.t3 = requestType;
 
             if (!sessionToken) {
-              _context.next = 10;
+              _context.next = 15;
               break;
             }
 
-            _context.next = 7;
+            _context.next = 12;
             return userLoaderBySessionToken.load(sessionToken);
 
-          case 7:
+          case 12:
             _context.t4 = _context.sent.id;
-            _context.next = 11;
+            _context.next = 16;
             break;
 
-          case 10:
+          case 15:
             _context.t4 = undefined;
 
-          case 11:
+          case 16:
             _context.t5 = _context.t4;
             _context.t6 = {
               appVersion: _context.t2,
@@ -50,7 +66,7 @@ var logUserRequest = function () {
             _context.t8 = sessionToken;
             return _context.abrupt('return', _context.t0.create.call(_context.t0, _context.t7, null, _context.t8));
 
-          case 16:
+          case 21:
           case 'end':
             return _context.stop();
         }
--- src/mutation/RequestLogHelper.js
+++ src/mutation/RequestLogHelper.js
@@ -3,11 +3,18 @@
 import { RequestLogService } from '@fingermenu/parse-server-common';
 import { Map } from 'immutable';
 
-const logUserRequest = async ({ appVersion }, requestType, { userLoaderBySessionToken }, sessionToken) =>
-  new RequestLogService().create(
+const logUserRequest = async ({ appVersion }, requestType, { userLoaderBySessionToken, configLoader }, sessionToken) => {
+  const enableLogRequest = await configLoader.load('enableLogRequest');
+
+  if (!enableLogRequest) {
+    return;
+  }
+
+  return new RequestLogService().create(
     Map({ appVersion, requestType, userId: sessionToken ? (await userLoaderBySessionToken.load(sessionToken)).id : undefined }),
     null,
     sessionToken,
   );
+};
 
 export default logUserRequest;
